c88d716181e88c682a83f2a249fce9f5f20617f4
Denys Pitel <nk120788pda@gmail.com>
2021-12-14 22:43:52 +0200

    finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+btn.addEventListener('click', onBtnClick)
+
+function onBtnClick () {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = `${document.body.style.backgroundColor}`;
+}
